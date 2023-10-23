27d69f312791df24caee7479b337a16ddcd5bda2
CoreAnujDixit <anujdixitdseu@gmail.com>
2023-10-23 14:25:58 +0530

    starting with signup page

--- src/Comps/Loginform.js
+++ src/Comps/Loginform.js
@@ -1,20 +1,18 @@
 import React from "react";
+import { AiFillLock, AiFillUnlock } from "react-icons/ai";
 
 const Loginform = () => {
-    
   const [fromdata, setfromdata] = useState({
     email: "",
     password: "",
   });
-  const [showPassword, setshowPassword] = useState(false)
-  const changeHandler = (event) =>{
-        setfromdata((prevData) => (
-            {
-                 ...prevData,
-                 [event.target.name] : event.target.value
-            }
-        ))
-  }
+  const [showPassword, setshowPassword] = useState(false);
+  const changeHandler = (event) => {
+    setfromdata((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
+  };
   return (
     <div>
       <form action="">
@@ -22,16 +20,34 @@ const Loginform = () => {
           <p>
             Email <sup>*</sup>
           </p>
-          <input type="text" required value={formdata.email} onClick={changeHandler} placeholder="Email ID"/>
+          <input
+            type="email"
+            required
+            value={formdata.email}
+            onClick={changeHandler}
+            placeholder="Email ID"
+            name="email"
+          />
         </label>
 
         <label htmlFor="">
           <p>
             Password <sup>*</sup>
           </p>
-          <input type={showPassword? ("text") : ("password") } required value={formdata.password} onClick={changeHandler} placeholder="Password"/>
-          <span></span>
+          <input
+            type={showPassword ? "text" : "password"}
+            required
+            value={formdata.password}
+            onClick={changeHandler}
+            placeholder="Password"
+            name="password"
+          />
+          <span onClick={() => setshowPassword((prev) => !prev)}>
+            {showPassword ? <AiFillUnlock /> : <AiFillLock />}
+          </span>
+          <p>Forget Password</p>
         </label>
+        <button>SignIn</button>
       </form>
     </div>
   );
